61f396d9865e8f014a1c1cd336a3794f65260bd1
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-04-29 15:37:36 +0300

    Add readonly properties for Recipe inputs and outputs

--- Project Show Off/Assets/Scripts/Runtime/Data/ScriptableObjects/CraftingRecipe.cs
+++ Project Show Off/Assets/Scripts/Runtime/Data/ScriptableObjects/CraftingRecipe.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Runtime.Data {
     [CreateAssetMenu(fileName = "NewCraftingRecipe", menuName = "Data/Crafting Recipe", order = 0)]
     public class CraftingRecipe : ScriptableObject {
-        [SerializeField] private List<ItemStack> Inputs;
-        [SerializeField] private List<ItemStack> Outputs;
+        [SerializeField] private List<ItemStack> inputs;
+        [SerializeField] private List<ItemStack> outputs;
+
+        public IReadOnlyList<ItemStack> Inputs => inputs.AsReadOnly();
+        public IReadOnlyList<ItemStack> Outputs => outputs.AsReadOnly();
     }
 }
