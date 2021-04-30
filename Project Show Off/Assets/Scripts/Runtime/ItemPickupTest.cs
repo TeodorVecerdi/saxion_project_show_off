67c2e95b345771b79819b8f1c802e9b288cdcddb
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-04-30 14:28:37 +0300

    Implement MaterialPickupEvent

--- Project Show Off/Assets/Scripts/Runtime/ItemPickupTest.cs
+++ Project Show Off/Assets/Scripts/Runtime/ItemPickupTest.cs
@@ -1,42 +1,37 @@
-using System;
-using Runtime;
+using Runtime.Event;
 using TMPro;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerInventory))]
-public class ItemPickupTest : MonoBehaviour {
-    [SerializeField] private TextMeshProUGUI text;
-    [SerializeField] private Transform cameraTransform;
-    
-    private Pickup pickupUnderMouse;
-    private PlayerInventory playerInventory;
+namespace Runtime {
+    public class ItemPickupTest : MonoBehaviour {
+        [SerializeField] private EventQueue eventQueue;
+        [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private Transform cameraTransform;
+        private Pickup pickupUnderMouse;
 
-    private void Awake() {
-        playerInventory = GetComponent<PlayerInventory>();
-    }
+        private void Update() {
+            if (transform.hasChanged) {
+                transform.hasChanged = false;
+                DoPickupRaycast();
+            }
 
-    private void Update() {
-        if (transform.hasChanged) {
-            transform.hasChanged = false;
-            DoPickupRaycast();
+            if (pickupUnderMouse != null && Input.GetKeyDown(KeyCode.F)) {
+                Destroy(pickupUnderMouse.gameObject);
+                eventQueue.QueueEvent(new MaterialPickedUpEvent(this, pickupUnderMouse.ItemStack));
+                pickupUnderMouse = null;
+                DoPickupRaycast();
+            }
         }
 
-        if (pickupUnderMouse != null && Input.GetKeyDown(KeyCode.F)) {
-            Destroy(pickupUnderMouse.gameObject);
-            playerInventory.AddMaterial(pickupUnderMouse.ItemStack);
-            pickupUnderMouse = null;
-            DoPickupRaycast();
-        }
-    }
-
-    private void DoPickupRaycast() {
-        var ray = new Ray(cameraTransform.position, cameraTransform.forward);
-        if (Physics.Raycast(ray, out var hitInfo, 10, LayerMask.GetMask("Pickup"))) {
-            pickupUnderMouse = hitInfo.transform.GetComponent<Pickup>();
-            text.text = pickupUnderMouse.ItemStack.Item.ItemName;
-        } else {
-            pickupUnderMouse = null;
-            text.text = "None";
+        private void DoPickupRaycast() {
+            var ray = new Ray(cameraTransform.position, cameraTransform.forward);
+            if (Physics.Raycast(ray, out var hitInfo, 10, LayerMask.GetMask("Pickup"))) {
+                pickupUnderMouse = hitInfo.transform.GetComponent<Pickup>();
+                text.text = pickupUnderMouse.ItemStack.Item.ItemName;
+            } else {
+                pickupUnderMouse = null;
+                text.text = "None";
+            }
         }
     }
 }
