1eb3f439a6143ca452a6209c7124140a97075b56
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-10 13:49:32 +0300

    Open and populate crafting UI

--- Project Show Off/Assets/Scripts/Runtime/Crafting/CrafterView.cs
+++ Project Show Off/Assets/Scripts/Runtime/Crafting/CrafterView.cs
@@ -1,32 +1,56 @@
-﻿using DG.Tweening;
+﻿using NaughtyAttributes;
 using UnityEngine;
 
 namespace Runtime {
     public class CrafterView : MonoBehaviour {
-        [Header("References")]
-        [SerializeField] private RectTransform main;
-        [SerializeField] private CanvasGroup fadeMain;
-
-        [Header("Settings")]
-        [SerializeField] private float moveDuration = 0.25f;
-        [SerializeField] private float fadeDuration = 0.25f;
-
-        private Tweener moveTweener;
-        private Tweener fadeTweener;
+        [Header("References"), SerializeField] private RectTransform recipeContainer;
+        [SerializeField] private GameObject mainUI;
+        [SerializeField] private CraftingRecipeView recipePrefab;
+        [SerializeField, ReadOnly] private bool isMenuOpen;
+        private Crafter currentCrafter;
+        private PlayerInventory playerInventory;
         
-        public void OpenView() {
-            main.gameObject.SetActive(true);
-            if(moveTweener is {active: true}) moveTweener.Kill();
-            if(fadeTweener is {active: true}) moveTweener.Kill();
-            moveTweener = main.DOAnchorPosY(0.77f, moveDuration);
-            fadeTweener = fadeMain.DOFade(1.0f, fadeDuration);
+        public bool IsMenuOpen => isMenuOpen;
+        
+        public void OpenView(Crafter crafter, PlayerInventory inventory) {
+            isMenuOpen = true;
+            currentCrafter = crafter;
+            playerInventory = inventory;
+            
+            UIHintController.Instance.RequestHide(this);
+            mainUI.SetActive(true);
+            LoadUI();
         }
 
-        public void CloseView() {
-            if(moveTweener is {active: true}) moveTweener.Kill();
-            if(fadeTweener is {active: true}) moveTweener.Kill();
-            moveTweener = main.DOAnchorPosY(-0.05f, moveDuration).OnComplete(() => main.gameObject.SetActive(false));
-            fadeTweener = fadeMain.DOFade(0.0f, fadeDuration);
+        private void CloseView() {
+            isMenuOpen = false;
+            mainUI.SetActive(false);
+            UIHintController.Instance.ReleaseHide(this);
+            // Todo: Implement pooling
+            while (recipeContainer.childCount > 0) {
+                var child = recipeContainer.GetChild(0);
+                child.SetParent(null);
+                Destroy(child.gameObject);
+            }
+        }
+
+        private void LoadUI() {
+            if (currentCrafter == null || playerInventory == null) {
+                Debug.LogError("Attempting to load crafter UI without a crafter or player inventory");
+                CloseView();
+                return;
+            }
+
+            // Todo: Implement pooling
+            foreach (var recipe in currentCrafter.Recipes) {
+                var recipeView = Instantiate(recipePrefab, recipeContainer);
+                recipeView.Build(recipe, playerInventory.MaterialInventory);
+            }
+        }
+
+        public void OnCloseButtonClicked() {
+            Debug.Log("OnCloseButtonClicked");
+            CloseView();
         }
     }
 }
