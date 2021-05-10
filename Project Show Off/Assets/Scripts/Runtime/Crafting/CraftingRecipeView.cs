bad9199a1597926c4edfb7b56b6b4d90a2417540
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-10 13:30:43 +0300

    Add crafting recipe views

---
+++ Project Show Off/Assets/Scripts/Runtime/Crafting/CraftingRecipeView.cs
@@ -0,0 +1,26 @@
+﻿using Runtime.Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runtime {
+    public class CraftingRecipeView : MonoBehaviour {
+        [Header("References")]
+        [SerializeField] private Image resultSprite;
+        [SerializeField] private TextMeshProUGUI resultCount;
+        [SerializeField] private TextMeshProUGUI resultName;
+        [Space, SerializeField] private RectTransform ingredientsContainer;
+        [SerializeField] private CraftingRecipeItemView itemPrefab;
+        
+        public void Build(CraftingRecipe recipe, Inventory materialInventory) {
+            foreach (var recipeIngredient in recipe.Ingredients) {
+                var recipeBuilder = Instantiate(itemPrefab, ingredientsContainer);
+                recipeBuilder.Build(recipeIngredient, materialInventory);
+            }
+
+            resultSprite.sprite = recipe.Result.Item.ItemSprite;
+            resultName.text = recipe.Result.Item.ItemName;
+            resultCount.text = $"x{recipe.Result.Count}";
+        }
+    }
+}
