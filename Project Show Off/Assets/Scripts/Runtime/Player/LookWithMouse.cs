f4accc5a7f4b66837e19ef7f34b55fcb40c091e0
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-12 15:36:37 +0300

    Refactor and optimize player movement

--- Project Show Off/Assets/Scripts/Runtime/Player/LookWithMouse.cs
+++ Project Show Off/Assets/Scripts/Runtime/Player/LookWithMouse.cs
@@ -1,29 +1,29 @@
-using System;
-using Runtime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class LookWithMouse : MonoBehaviour {
-    [SerializeField] private float mouseSensitivity = 100f;
-    [SerializeField] private Transform playerBody;
+namespace Runtime {
+    public class LookWithMouse : MonoBehaviour {
+        [SerializeField] private float mouseSensitivity = 100f;
+        [SerializeField] private Transform playerBody;
 
-    private float xRotation;
+        private float xRotation;
 
-    internal static void SetMouseLock(bool locked) {
-        Cursor.lockState = locked ? CursorLockMode.Locked : CursorLockMode.None;
-    }
+        internal static void SetMouseLock(bool locked) {
+            Cursor.lockState = locked ? CursorLockMode.Locked : CursorLockMode.None;
+        }
 
-    private void Start() {
-        SetMouseLock(true);
-    }
+        private void Start() {
+            SetMouseLock(true);
+        }
 
-    private void Update() {
-        if(InputManager.PlayerActions.Look.phase != InputActionPhase.Started) return;
+        private void Update() {
+            if(InputManager.PlayerActions.Look.phase != InputActionPhase.Started) return;
         
-        var delta = InputManager.MouseDelta * (mouseSensitivity * Time.deltaTime * 0.06666667f);
-        xRotation -= delta.y;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * delta.x);
+            var delta = InputManager.MouseDelta * (mouseSensitivity * Time.deltaTime * 0.06666667f);
+            xRotation -= delta.y;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * delta.x);
+        }
     }
 }
--- Project Show Off/Assets/Scripts/Runtime/Player/PlayerMovement.cs
+++ Project Show Off/Assets/Scripts/Runtime/Player/PlayerMovement.cs
@@ -1,73 +1,33 @@
-using Runtime;
-using UnityCommons;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoSingleton<PlayerMovement> {
-    [SerializeField] private CharacterController controller;
+namespace Runtime {
+    public class PlayerMovement : MonoBehaviour {
 
-    [SerializeField] private float speed = 12f;
-    [SerializeField] private float gravity = -10f;
-    [SerializeField] private float jumpHeight = 2f;
+        [SerializeField] private float speed;
+        [SerializeField] private float gravity;
+        [SerializeField] private float jumpHeight;
 
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private float groundDistance = 0.4f;
-    [SerializeField] private LayerMask groundMask;
+        private CharacterController controller;
+        private Vector3 velocity;
 
-    private bool isGrounded;
-    private bool isEnabled;
-    private InputAction jump;
-    private InputAction movement;
-    private Vector3 velocity;
-
-    public void SetEnabled(bool state) {
-        isEnabled = state;
-    }
-
-    private void Start() {
-        SetEnabled(true);
-
-        movement = new InputAction("PlayerMovement", binding: "<Gamepad>/leftStick");
-        movement.AddCompositeBinding("Dpad")
-                .With("Up", "<Keyboard>/w")
-                .With("Up", "<Keyboard>/upArrow")
-                .With("Down", "<Keyboard>/s")
-                .With("Down", "<Keyboard>/downArrow")
-                .With("Left", "<Keyboard>/a")
-                .With("Left", "<Keyboard>/leftArrow")
-                .With("Right", "<Keyboard>/d")
-                .With("Right", "<Keyboard>/rightArrow");
-
-        jump = new InputAction("PlayerJump", binding: "<Gamepad>/a");
-        jump.AddBinding("<Keyboard>/space");
-
-        movement.Enable();
-        jump.Enable();
-    }
-
-    // Update is called once per frame
-    private void Update() {
-        var x = 0.0f;
-        var z = 0.0f;
-        var jumpPressed = false;
-        
-        if (isEnabled) {
-            var delta = movement.ReadValue<Vector2>();
-            x = delta.x;
-            z = delta.y;
-            jumpPressed = Mathf.Approximately(jump.ReadValue<float>(), 1);
+        private void Awake() {
+            controller = GetComponent<CharacterController>();
         }
 
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y < 0) velocity.y = -2f;
+        private void Update() {
+            var grounded = controller.isGrounded;
+            if (grounded && velocity.y < 0.0f) velocity.y = 0.0f;
 
-        var playerTransform = transform; // reason: repeated property access of built in component
-        var move = playerTransform.right * x + playerTransform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+            var delta = InputManager.PlayerMovement;
+            var playerTransform = transform; // reason: repeated property access of built in component
+            var movement = playerTransform.right * delta.x + playerTransform.forward * delta.y;
+            controller.Move(speed * Time.deltaTime * movement);
 
-        if (jumpPressed && isGrounded) velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
+            if (InputManager.WasJumpTriggered && grounded) 
+                velocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
+        
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
     }
 }
