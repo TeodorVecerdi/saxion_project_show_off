49216ded6aea94ea530ce180bd8ab84309429aa7
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-13 13:45:20 +0300

    Make InputManager internal

--- Project Show Off/Assets/Scripts/Runtime/Miscellaneous/InputManager.cs
+++ Project Show Off/Assets/Scripts/Runtime/Miscellaneous/InputManager.cs
@@ -2,7 +2,7 @@
 using UnityEngine.InputSystem;
 
 namespace Runtime {
-    public static class InputManager {
+    internal static class InputManager {
         private static DefaultInputActions actions;
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
@@ -15,28 +15,28 @@ namespace Runtime {
             Enable();
         }
 
-        public static DefaultInputActions.PlayerActions PlayerActions => actions.Player;
-        public static DefaultInputActions.UIActions UIActions => actions.UI;
-        public static DefaultInputActions.BuildModeActions BuildModeActions => actions.BuildMode;
+        internal static DefaultInputActions.PlayerActions PlayerActions => actions.Player;
+        internal static DefaultInputActions.UIActions UIActions => actions.UI;
+        internal static DefaultInputActions.BuildModeActions BuildModeActions => actions.BuildMode;
         
         // PlayerActions
-        public static Vector2 PlayerMovement => PlayerActions.Move.ReadValue<Vector2>(); 
-        public static bool WasJumpTriggered => PlayerActions.Jump.triggered;
-        public static bool WasPickupTriggered => PlayerActions.PickUp.triggered;
-        public static bool WasOpenMenuTriggered => PlayerActions.OpenMenu.triggered;
-        public static Vector2 MouseDelta => PlayerActions.Look.ReadValue<Vector2>();
+        internal static Vector2 PlayerMovement => PlayerActions.Move.ReadValue<Vector2>(); 
+        internal static bool WasJumpTriggered => PlayerActions.Jump.triggered;
+        internal static bool WasPickupTriggered => PlayerActions.PickUp.triggered;
+        internal static bool WasOpenMenuTriggered => PlayerActions.OpenMenu.triggered;
+        internal static Vector2 MouseDelta => PlayerActions.Look.ReadValue<Vector2>();
         
         // BuildModeActions
-        public static Vector2 CameraKeyboardPan => BuildModeActions.KeyboardPan.ReadValue<Vector2>();
-        public static Vector2 Zoom => BuildModeActions.Zoom.ReadValue<Vector2>();
-        public static bool IsBoosting => BuildModeActions.Boost.phase == InputActionPhase.Started;
+        internal static Vector2 CameraKeyboardPan => BuildModeActions.KeyboardPan.ReadValue<Vector2>();
+        internal static Vector2 Zoom => BuildModeActions.Zoom.ReadValue<Vector2>();
+        internal static bool IsBoosting => BuildModeActions.Boost.phase == InputActionPhase.Started;
         
         
-        public static void Enable() {
+        internal static void Enable() {
             actions.Enable();
         }
 
-        public static void Disable() {
+        internal static void Disable() {
             actions.Disable();
         }
     }
