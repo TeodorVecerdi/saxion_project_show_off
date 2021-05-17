e6403b91097f2c6b06c5b647e2223506486d589e
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-17 13:27:34 +0300

    Remove unnecessary dependency on PlayerInventory from Crafter

--- Project Show Off/Assets/Scripts/Runtime/Crafting/Crafter.cs
+++ Project Show Off/Assets/Scripts/Runtime/Crafting/Crafter.cs
@@ -11,7 +11,6 @@ namespace Runtime {
         [SerializeField] private CrafterView crafterView;
         [SerializeField] private KeyCode openCrafterKey = KeyCode.F;
         
-        private PlayerInventory playerInventory;
         private bool canOpenMenu;
 
         public List<CraftingRecipe> Recipes => recipes;
@@ -26,21 +25,19 @@ namespace Runtime {
 
         private void OpenMenuPerformed(InputAction.CallbackContext context) {
             if(!canOpenMenu || crafterView.IsMenuOpen) return;
-            crafterView.OpenView(this, playerInventory);
+            crafterView.OpenView(this);
         }
 
         private void OnTriggerEnter(Collider other) {
             if(!other.CompareTag("Player")) return;
             canOpenMenu = true;
             UIHintController.Instance.Add(string.Format(kOpenCrafterHintFormat, openCrafterKey));
-            playerInventory = other.GetComponent<PlayerInventory>();
         }
 
         private void OnTriggerExit(Collider other) {
             if(!other.CompareTag("Player")) return;
             canOpenMenu = false;
             UIHintController.Instance.Remove(string.Format(kOpenCrafterHintFormat, openCrafterKey));
-            playerInventory = null;
         }
     }
 }
