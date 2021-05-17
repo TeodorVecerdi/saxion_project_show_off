e4552e8832b4cdda4781f4166fa7b2296c6124e4
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-17 12:04:26 +0300

    Implement game mode toggle to player movement

--- Project Show Off/Assets/Scripts/Runtime/Player/PlayerMovement.cs
+++ Project Show Off/Assets/Scripts/Runtime/Player/PlayerMovement.cs
@@ -1,7 +1,9 @@
+using Runtime.Event;
 using UnityEngine;
+using EventType = Runtime.Event.EventType;
 
 namespace Runtime {
-    public class PlayerMovement : MonoBehaviour {
+    public class PlayerMovement : MonoBehaviour, IEventSubscriber {
 
         [SerializeField] private float speed;
         [SerializeField] private float gravity;
@@ -10,6 +12,7 @@ namespace Runtime {
 
         private CharacterController controller;
         private Vector3 velocity;
+        private bool isEnabled;
 
         private void Awake() {
             controller = GetComponent<CharacterController>();
@@ -19,7 +22,7 @@ namespace Runtime {
             var grounded = controller.isGrounded;
             if (grounded && velocity.y < 0.0f) velocity.y = 0.0f;
 
-            var delta = InputManager.PlayerMovement * speed * Time.deltaTime;
+            var delta = speed * Time.deltaTime * InputManager.PlayerMovement;
             var movement = cameraTransform.right * delta.x + cameraTransform.forward * delta.y;
             movement.y = 0.0f;
             controller.Move(movement);
@@ -29,6 +32,27 @@ namespace Runtime {
         
             velocity.y += gravity * Time.deltaTime;
             controller.Move(velocity * Time.deltaTime);
+        }
+
+        /// <summary>
+        /// <para>Receives an event from the Event Queue</para>
+        /// </summary>
+        /// <param name="eventData">Event data raised</param>
+        /// <returns><c>true</c> if event propagation should be stopped, <c>false</c> otherwise.</returns>
+        public bool OnEvent(EventData eventData) {
+            switch (eventData) {
+                case EmptyEvent {Type: EventType.GameModeToggle}: {
+                    ToggleEnabledState();
+                    return false;
+                }
+                default: return false;
+            }
+        }
+
+        private void ToggleEnabledState() {
+            isEnabled = !isEnabled;
+            if(isEnabled) InputManager.PlayerActions.Enable();
+            else InputManager.PlayerActions.Disable();
         }
     }
 }
