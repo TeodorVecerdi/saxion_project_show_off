9a549e493604af4d8261d625be7236fd20e57e8d
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-17 12:32:57 +0300

    Remove copy-pasted comment from class

--- Project Show Off/Assets/Scripts/Runtime/Event/Events/ChangeMouseLockEvent.cs
+++ Project Show Off/Assets/Scripts/Runtime/Event/Events/ChangeMouseLockEvent.cs
@@ -1,7 +1,4 @@
 ﻿namespace Runtime.Event {
-    /// <summary>
-    /// Serves as an empty event that can be of any type. Useful when you don't need extra data and just want to trigger an event of a specific type
-    /// </summary>
     public sealed class ChangeMouseLockEvent : EventData {
         public override EventType Type => EventType.ChangeMouseLock;
         public bool State { get; }
