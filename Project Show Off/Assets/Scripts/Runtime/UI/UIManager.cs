d59ada6ebaf5084774f1784c8a21a4b66c59fa90
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-17 12:58:23 +0300

    Add UI Manager script for toggling between game/build UI's

---
+++ Project Show Off/Assets/Scripts/Runtime/UI/UIManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Runtime.Event;
+using UnityEngine;
+using EventType = Runtime.Event.EventType;
+
+namespace Runtime {
+    public class UIManager : MonoBehaviour, IEventSubscriber {
+        [SerializeField] private List<GameObject> gameModeObjects;
+        [SerializeField] private List<GameObject> buildModeObjects;
+        
+        private IDisposable gameModeToggleEventUnsubscriber;
+        private bool isBuildModeActive;
+
+        private void Awake() {
+            gameModeToggleEventUnsubscriber = EventQueue.Subscribe(this, EventType.GameModeToggle);
+        }
+
+        private void Start() {
+            DisableBuildModeUI();
+        }
+
+        private void OnDestroy() {
+            gameModeToggleEventUnsubscriber.Dispose();
+        }
+
+        private void EnableBuildModeUI() {
+            gameModeObjects.ForEach(obj => obj.SetActive(false));
+            buildModeObjects.ForEach(obj => obj.SetActive(true));
+        }
+
+        private void DisableBuildModeUI() {
+            gameModeObjects.ForEach(obj => obj.SetActive(true));
+            buildModeObjects.ForEach(obj => obj.SetActive(false));
+        }
+
+        private void OnGameModeToggle() {
+            isBuildModeActive = !isBuildModeActive;
+            if (isBuildModeActive) EnableBuildModeUI();
+            else DisableBuildModeUI();
+        }
+
+        /// <summary>
+        /// <para>Receives an event from the Event Queue</para>
+        /// </summary>
+        /// <param name="eventData">Event data raised</param>
+        /// <returns><c>true</c> if event propagation should be stopped, <c>false</c> otherwise.</returns>
+        public bool OnEvent(EventData eventData) {
+            switch (eventData) {
+                case EmptyEvent {Type: EventType.GameModeToggle}: {
+                    OnGameModeToggle();
+                    return false;
+                }
+                default: return false;
+            }
+        }
+    }
+}
