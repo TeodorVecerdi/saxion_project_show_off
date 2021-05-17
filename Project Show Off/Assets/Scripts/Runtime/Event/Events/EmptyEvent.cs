6cfbd960cad5d1f1fbc86f66a5f00062eb9664db
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-17 12:11:28 +0300

    Make EmptyEvent.cs sealed

--- Project Show Off/Assets/Scripts/Runtime/Event/Events/EmptyEvent.cs
+++ Project Show Off/Assets/Scripts/Runtime/Event/Events/EmptyEvent.cs
@@ -2,7 +2,7 @@
     /// <summary>
     /// Serves as an empty event that can be of any type. Useful when you don't need extra data and just want to trigger an event of a specific type
     /// </summary>
-    public class EmptyEvent : EventData {
+    public sealed class EmptyEvent : EventData {
         public EmptyEvent(object sender, EventType eventType) : base(sender) {
             Type = eventType;
         }
