97db173eb6b507a01691966cdaec06ee3dec2e59
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-18 17:13:38 +0300

    Rename MaterialPickedUpEvent.cs

---
+++ Project Show Off/Assets/Scripts/Runtime/Event/Events/ItemPickupEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Runtime.Event {
+    public sealed class ItemPickupEvent : EventData {
+        public override EventType Type { get; }
+        
+        public Pickup Pickup { get; }
+        
+        public ItemPickupEvent(object sender, EventType eventType, Pickup pickup) : base(sender) {
+            Pickup = pickup;
+            Type = eventType;
+        }
+    }
+}
