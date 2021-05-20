053d8ae4d24aef62a1102f62645ddedd4f75e1a9
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-20 17:50:31 +0300

    Add buildable object SO

---
+++ Project Show Off/Assets/Scripts/Runtime/Data/ScriptableObjects/BuildableObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Runtime.Data {
+    [CreateAssetMenu(fileName = "NewBuildableObject", menuName = "Data/BuildableObjects", order = 0)]
+    public class BuildableObject : ScriptableObject {
+        [SerializeField] private Sprite objectSprite;
+        [SerializeField] private BuildableObjectPreview prefab;
+
+        public Sprite ObjectSprite => objectSprite;
+        public BuildableObjectPreview Prefab => prefab;
+    }
+}
