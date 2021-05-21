8bb52832e1679ca35febd92c2020712289b28784
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-21 15:29:51 +0300

    Add pickup duration to trash

--- Project Show Off/Assets/Scripts/Runtime/Data/ScriptableObjects/Item.cs
+++ Project Show Off/Assets/Scripts/Runtime/Data/ScriptableObjects/Item.cs
@@ -7,10 +7,12 @@ namespace Runtime.Data {
         [SerializeField] private TrashCategory trashCategory;
         [SerializeField] private float minimumMass;
         [SerializeField] private float maximumMass;
+        [SerializeField, Tooltip("In seconds")] private float pickupDuration = 1.0f;
 
         public string ItemName => itemName;
         public TrashCategory TrashCategory => trashCategory;
         public float MinimumMass => minimumMass;
         public float MaximumMass => maximumMass;
+        public float PickupDuration => pickupDuration;
     }
 }
