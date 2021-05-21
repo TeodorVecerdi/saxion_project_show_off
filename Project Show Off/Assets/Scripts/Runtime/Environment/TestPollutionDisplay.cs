bf34b68f8c81f6283153db32f9ad03361b137b61
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-05-21 16:11:25 +0300

    Add pollution display

---
+++ Project Show Off/Assets/Scripts/Runtime/Environment/TestPollutionDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Runtime.Event;
+using TMPro;
+using UnityEngine;
+using EventType = Runtime.Event.EventType;
+
+namespace Runtime {
+    public class TestPollutionDisplay : MonoBehaviour, IEventSubscriber {
+        [SerializeField] private TextMeshProUGUI pollutionText;
+        private IDisposable pollutionUpdateEventUnsubscribeToken;
+        
+        private void Awake() {
+            pollutionUpdateEventUnsubscribeToken = EventQueue.Subscribe(this, EventType.PollutionUpdate);
+        }
+
+        private void OnDestroy() {
+            pollutionUpdateEventUnsubscribeToken.Dispose();
+        }
+
+        /// <summary>
+        /// <para>Receives an event from the Event Queue</para>
+        /// </summary>
+        /// <param name="eventData">Event data raised</param>
+        /// <returns><c>true</c> if event propagation should be stopped, <c>false</c> otherwise.</returns>
+        public bool OnEvent(EventData eventData) {
+            switch (eventData) {
+                case PollutionUpdateEvent pollutionUpdateEvent: {
+                    pollutionText.text = $"Pollution: {pollutionUpdateEvent.Pollution:F1}";
+                    return false;
+                }
+                default: return false;
+            }
+        }
+    }
+}
