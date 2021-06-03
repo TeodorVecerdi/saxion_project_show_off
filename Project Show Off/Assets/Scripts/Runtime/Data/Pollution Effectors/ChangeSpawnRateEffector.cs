c689fd9c3d23281d5ba2dba2ef74e3684afcc510
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-06-03 14:24:13 +0300

    Add VFX Pollution Effectors

---
+++ Project Show Off/Assets/Scripts/Runtime/Data/Pollution Effectors/ChangeSpawnRateEffector.cs
@@ -0,0 +1,22 @@
+﻿using UnityCommons;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace Runtime.Data {
+    [CreateAssetMenu(fileName = "NewSpawnRateEffector", menuName = "Data/VFX Pollution/Change Spawn Rate", order = 0)]
+    public class ChangeSpawnRateEffector : VFXPollutionEffector {
+        [SerializeField] private AnimationCurve curve;
+        [SerializeField] private bool invertCurve;
+        [SerializeField] private float maxValue = 50.0f;
+        
+        private static readonly int spawnRateId = Shader.PropertyToID("Spawn Rate");
+        
+        public override void Apply(VisualEffect effect, float rawPollution, float pollutionRatio) {
+            var time = pollutionRatio.Clamped01();
+            if (invertCurve) time = 1 - time;
+            var spawnRate = (curve.Evaluate(time) * maxValue).Clamped(0.0f, maxValue);
+            
+            effect.SetFloat(spawnRateId, spawnRate);
+        }
+    }
+}
---
+++ Project Show Off/Assets/Scripts/Runtime/Data/Pollution Effectors/VFXPollutionEffector.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace Runtime.Data {
+    public abstract class VFXPollutionEffector : ScriptableObject {
+        public abstract void Apply(VisualEffect effect, float rawPollution, float pollutionRatio);
+    }
+}
