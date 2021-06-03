6916afc6845d64fd584d9d878e3d8e0b1a558ff9
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-06-03 15:54:12 +0300

    Update fence script and add hedge/bush script

--- Project Show Off/Assets/Scripts/Runtime/Tools/FencePlacer.cs
+++ Project Show Off/Assets/Scripts/Runtime/Tools/FencePlacer.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using NaughtyAttributes;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
 
 namespace Runtime.Tools {
     public class FencePlacer : MonoBehaviour {
@@ -10,7 +14,7 @@ namespace Runtime.Tools {
         private const float fenceSpacing = 1.757f;
         private static readonly Vector3 initialDelta = initialSpacing * Vector3.forward;
         private static readonly Vector3 fenceDelta = fenceSpacing * Vector3.forward;
-        
+
         [SerializeField, HideInInspector] private GameObject fenceOpenPrefab;
         [SerializeField, HideInInspector] private GameObject fenceClosedPrefab;
         [SerializeField, HideInInspector] private GameObject fenceClosed;
@@ -18,11 +22,12 @@ namespace Runtime.Tools {
 
         [Button]
         private void UpdateFences() {
+#if UNITY_EDITOR
             if (fenceOpenPrefab == null || fenceClosedPrefab == null) {
                 fenceOpenPrefab = Resources.Load<GameObject>("Prefabs/Environment/Fence Open");
                 fenceClosedPrefab = Resources.Load<GameObject>("Prefabs/Environment/Fence Closed");
             }
-            
+
             openFences.ForEach(DestroyImmediate);
             openFences.Clear();
             if (count == 0) {
@@ -30,21 +35,23 @@ namespace Runtime.Tools {
                 fenceClosed = null;
             } else {
                 if (fenceClosed == null) {
-                    fenceClosed = Instantiate(fenceClosedPrefab, transform, false);
+                    fenceClosed = PrefabUtility.InstantiatePrefab(fenceClosedPrefab, transform) as GameObject;
+                    fenceClosed!.transform.localPosition = Vector3.zero;
                 }
 
                 if (count > 1) {
-                    var fenceOpen = Instantiate(fenceOpenPrefab, transform, false);
-                    fenceOpen.transform.localPosition = initialDelta;
+                    var fenceOpen =  PrefabUtility.InstantiatePrefab(fenceOpenPrefab, transform) as GameObject;
+                    fenceOpen!.transform.localPosition = initialDelta;
                     openFences.Add(fenceOpen);
                 }
-                
+
                 for (var i = 2; i < count; i++) {
-                    var fenceOpen = Instantiate(fenceOpenPrefab, transform, false);
-                    fenceOpen.transform.localPosition = initialDelta + fenceDelta * (i-1);
+                    var fenceOpen =  PrefabUtility.InstantiatePrefab(fenceOpenPrefab, transform) as GameObject;
+                    fenceOpen!.transform.localPosition = initialDelta + fenceDelta * (i - 1);
                     openFences.Add(fenceOpen);
                 }
             }
+#endif
         }
     }
 }
---
+++ Project Show Off/Assets/Scripts/Runtime/Tools/HedgePlacer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
+
+namespace Runtime.Tools {
+    public class HedgePlacer : MonoBehaviour {
+        [SerializeField, Min(0)] private int count;
+        [SerializeField] private float spacing = 0.0f;
+
+        private const float hedgeSpacing = 2.72f;
+
+        [SerializeField, HideInInspector] private GameObject hedgePrefab;
+        [SerializeField, HideInInspector] private List<GameObject> hedges;
+
+        [Button]
+        private void UpdateFences() {
+#if UNITY_EDITOR
+            if (hedgePrefab == null) {
+                hedgePrefab = Resources.Load<GameObject>("Prefabs/Environment/Hedge");
+            }
+
+            hedges.ForEach(DestroyImmediate);
+            hedges.Clear();
+            
+            for (var i = 0; i < count; i++) {
+                var hedge =  PrefabUtility.InstantiatePrefab(hedgePrefab, transform) as GameObject;
+                hedge!.transform.localPosition = (spacing * i + hedgeSpacing * i) * Vector3.forward;
+                hedges.Add(hedge);
+            }
+#endif
+        }
+    }
+}
