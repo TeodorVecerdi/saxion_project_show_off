33910f242c9e29aca1697072d92b9cc748044201
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-06-07 16:10:48 +0300

    Add controller script that loads up the current tutorial on startup

--- Project Show Off/Assets/Scripts/Runtime/UI/Tutorial/TutorialController.cs
+++ Project Show Off/Assets/Scripts/Runtime/UI/Tutorial/TutorialController.cs
@@ -1,7 +1,49 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using NaughtyAttributes;
+using UnityEngine;
 
 namespace Runtime.Tutorial {
     public class TutorialController : MonoBehaviour {
-        [SerializeField] private TutorialSlide startingTutorial;
+        [SerializeField] private List<TutorialSlide> allTutorials;
+        [SerializeField] private float transitionFromX;
+        [SerializeField] private float transitionToX;
+        
+        private Dictionary<string, TutorialSlide> tutorialDictionary;
+        private TutorialSlide activeTutorialSlide;
+
+        [Button]
+        public void ResetTutorials() {
+            PlayerPrefs.DeleteKey("current_tutorial");
+        }
+
+        private void Awake() {
+            tutorialDictionary = new Dictionary<string, TutorialSlide>();
+            foreach (var tutorialSlide in allTutorials) {
+                tutorialDictionary.Add(tutorialSlide.TutorialKey, tutorialSlide);
+                tutorialSlide.LoadTransitionSettings(transitionFromX, transitionToX);
+                
+                // ensure all slides are disabled
+                tutorialSlide.gameObject.SetActive(false);
+            }
+
+            var currentTutorial = PlayerPrefs.GetString("current_tutorial");
+            if (string.IsNullOrEmpty(currentTutorial) || !tutorialDictionary.ContainsKey(currentTutorial)) {
+                activeTutorialSlide = allTutorials[0];
+            } else if (string.Equals(currentTutorial, "none", StringComparison.InvariantCulture)) {
+                activeTutorialSlide = null;
+            } else {
+                activeTutorialSlide = tutorialDictionary[currentTutorial];
+            }
+        }
+
+        private void Start() {
+            if(activeTutorialSlide == null) return;
+
+            activeTutorialSlide.GetComponent<RectTransform>().anchoredPosition = new Vector2(transitionToX, -8.0f);
+            activeTutorialSlide.gameObject.SetActive(true);
+            activeTutorialSlide.Show(2.0f);
+        }
     }
 }
