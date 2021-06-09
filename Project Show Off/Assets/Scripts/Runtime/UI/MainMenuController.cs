af2658a5adaf9a659358beaa56470c93968694dc
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-06-09 16:32:16 +0300

    Make main menu functional

---
+++ Project Show Off/Assets/Scripts/Runtime/UI/MainMenuController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Runtime {
+    public class MainMenuController : MonoBehaviour {
+        private static readonly int speedPropertyID = Animator.StringToHash("Speed");
+        
+        [SerializeField] private Image fadeImage;
+        [SerializeField] private AnimationClip clip;
+        
+        private Animator animator;
+
+        private void Awake() {
+            animator = GetComponent<Animator>();
+        }
+
+        public void OnPlayClicked() {
+            animator.SetFloat(speedPropertyID, 1.0f);
+
+            // prevents buttons from being clicked
+            fadeImage.raycastTarget = true;
+
+            StartCoroutine(SwitchSceneAfter(clip.length));
+        }
+
+        public void OnSettingsClicked() {
+            Debug.Log("Settings clicked");
+        }
+
+        public void OnExitClicked() {
+            Application.Quit();
+        }
+
+        private IEnumerator SwitchSceneAfter(float delay) {
+            Debug.Log($"Switching scene after {delay} seconds");
+            yield return new WaitForSeconds(delay);
+            var operation = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+            yield return operation;
+            if (!operation.isDone) Debug.Log("Loading not done");
+
+            var unloadOperation = SceneManager.UnloadSceneAsync(0);
+            yield return unloadOperation;
+        }
+    }
+}
