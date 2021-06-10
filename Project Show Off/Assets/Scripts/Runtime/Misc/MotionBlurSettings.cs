70e4eac25105ede94349e487c3f95aecfc669092
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-06-10 15:23:29 +0300

    Implement motion blur settings

---
+++ Project Show Off/Assets/Scripts/Runtime/Misc/MotionBlurSettings.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NaughtyAttributes;
+using Runtime.Event;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using EventType = Runtime.Event.EventType;
+
+namespace Runtime {
+    public class MotionBlurSettings : MonoBehaviour, IEventSubscriber {
+        [SerializeField, Required] private Volume globalSettingsVolume;
+
+        private MotionBlur motionBlurComponent;
+        private IDisposable settingsChangedEventUnsubscribeToken;
+        
+        private void Awake() {
+            if (globalSettingsVolume.profile.TryGet<MotionBlur>(out var motionBlur)) {
+                motionBlurComponent = motionBlur;
+                return;
+            }
+
+            settingsChangedEventUnsubscribeToken = this.Subscribe(EventType.SettingsChanged);
+            
+            // self-destroy if couldn't get motion blur component
+            Destroy(gameObject);
+        }
+
+        private void Start() {
+            UpdateMotionBlur(PlayerPrefs.GetInt("Settings_MotionBlur", 1) == 1);
+        }
+
+        private void OnDestroy() {
+            settingsChangedEventUnsubscribeToken.Dispose();
+        }
+
+        private void UpdateMotionBlur(bool enableMotionBlur) {
+            motionBlurComponent.active = enableMotionBlur;
+        }
+
+        /// <summary>
+        /// <para>Receives an event from the Event Queue</para>
+        /// </summary>
+        /// <param name="eventData">Event data raised</param>
+        /// <returns><c>true</c> if event propagation should be stopped, <c>false</c> otherwise.</returns>
+        public bool OnEvent(EventData eventData) {
+            switch (eventData) {
+                case SettingsChangedEvent settingsChangedEvent: {
+                    UpdateMotionBlur(settingsChangedEvent.EnableMotionBlur);
+                    return false;
+                }
+                
+                default: return false;
+            }
+        }
+    }
+}
