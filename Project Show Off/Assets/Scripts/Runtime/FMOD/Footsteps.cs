8b18d5736dfda65f2d5370ad2b213ef26b8e156c
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-06-16 12:43:26 +0200

    Tweak footsteps

--- Project Show Off/Assets/Scripts/Runtime/FMOD/Footsteps.cs
+++ Project Show Off/Assets/Scripts/Runtime/FMOD/Footsteps.cs
@@ -34,6 +34,7 @@
 /* ======================================================================================== */
 
 using UnityEngine;
+using UnityEngine.Serialization;
 
 //This script plays footstep sounds.
 //It will play a footstep sound after a set amount of distance travelled.
@@ -44,85 +45,84 @@ public class Footsteps : MonoBehaviour {
     //FMOD Studio variables
     //The FMOD Studio Event path.
     //This script is designed for use with an event that has a game parameter for each of the surface variables, but it will still compile and run if they are not present.
-    [FMODUnity.EventRef]
-    public string m_EventPath;
+    [FormerlySerializedAs("m_EventPath")] [FMODUnity.EventRef]
+    public string eventPath;
 
     //Surface variables
     //Range: 0.0f - 1.0f
     //These values represent the amount of each type of surface found when raycasting to the ground.
     //They are exposed to the UI (public) only to make it easy to see the values as the player moves through the scene.
-    public float m_Grass;
-    public float m_Gravel;
-    //public float m_Dirt;
-    //public float m_Sand;
+    private float grassAmount;
+    private float gravelAmount;
 
     //Step variables
     //These variables are used to control when the player executes a footstep.
     //This is very basic, and simply executes a footstep based on distance travelled.
     //Ideally, in this case, footsteps would be triggered based on the headbob script. Or if there was an animated player model it could be triggered from the animation system.
     //You could also add variation based on speed travelled, and whether the player is running or walking.
-    public float m_StepDistance = 2.0f;
-    float m_StepRand;
-    Vector3 m_PrevPos;
-    float m_DistanceTravelled;
+    [SerializeField] private float stepDistance = 2.0f;
+    [SerializeField] private float raycastDistance = 1.0f;
+    private float stepRand;
+    private Vector3 prevPos;
+    private float distanceTravelled;
 
     //Debug variables
-    //If m_Debug is true, this script will:
+    //If Debug is true, this script will:
     // - Draw a debug line to represent the ray that was cast into the ground.
     // - Draw the triangle of the mesh that was hit by the ray that was cast into the ground.
     // - Log the surface values to the console.
     // - Log to the console when an expected game parameter is not found in the FMOD Studio event.
-    public bool m_Debug;
-    Vector3 m_LinePos;
-    Vector3 m_TrianglePoint0;
-    Vector3 m_TrianglePoint1;
-    Vector3 m_TrianglePoint2;
+    [SerializeField] private bool debug;
+    private Vector3 linePos;
+    private Vector3 trianglePoint0;
+    private Vector3 trianglePoint1;
+    private Vector3 trianglePoint2;
 
     void Start() {
         //Initialise random, set seed
         Random.InitState(System.DateTime.Now.Second);
 
         //Initialise member variables
-        m_StepRand = Random.Range(0.0f, 0.5f);
-        m_LinePos = m_PrevPos = transform.position;
+        stepRand = Random.Range(0.0f, 0.5f);
+        linePos = prevPos = transform.position;
     }
 
     private void Update() {
-        m_DistanceTravelled += (transform.position - m_PrevPos).magnitude;
-        if (m_DistanceTravelled >= m_StepDistance + m_StepRand) //TODO: Play footstep sound based on position from headbob script
+        distanceTravelled += (transform.position - prevPos).magnitude;
+        if (distanceTravelled >= stepDistance + stepRand) //TODO: Play footstep sound based on position from headbob script
         {
             PlayFootstepSound();
-            m_StepRand = Random.Range(0.0f, 0.5f); //Adding subtle random variation to the distance required before a step is taken - Re-randomise after each step.
-            m_DistanceTravelled = 0.0f;
+            stepRand = Random.Range(0.0f, 0.5f); //Adding subtle random variation to the distance required before a step is taken - Re-randomise after each step.
+            distanceTravelled = 0.0f;
         }
 
-        m_PrevPos = transform.position;
+        prevPos = transform.position;
 
-        if (m_Debug) {
-            Debug.DrawLine(m_LinePos, m_LinePos + Vector3.down * 1000.0f);
-            Debug.DrawLine(m_TrianglePoint0, m_TrianglePoint1);
-            Debug.DrawLine(m_TrianglePoint1, m_TrianglePoint2);
-            Debug.DrawLine(m_TrianglePoint2, m_TrianglePoint0);
+        if (debug) {
+            Debug.DrawLine(linePos, linePos + Vector3.down * 1000.0f);
+            Debug.DrawLine(trianglePoint0, trianglePoint1);
+            Debug.DrawLine(trianglePoint1, trianglePoint2);
+            Debug.DrawLine(trianglePoint2, trianglePoint0);
         }
     }
 
     private void PlayFootstepSound() {
-        m_Grass = 1.0f;
-        m_Gravel = 0.0f;
+        grassAmount = 1.0f;
+        gravelAmount = 0.0f;
 
         var ray = new Ray(transform.position + Vector3.up * 0.5f, Vector3.down);
-        if (Physics.Raycast(ray, out var hit, 1000.0f, LayerMask.GetMask("Ground"))) {
-            if (m_Debug)
-                m_LinePos = transform.position;
+        if (Physics.Raycast(ray, out var hit, raycastDistance + 0.5f, LayerMask.GetMask("Ground"))) {
+            if (debug)
+                linePos = transform.position;
 
             var meshFilter = hit.collider.gameObject.GetComponent<MeshFilter>();
             var mesh = meshFilter.mesh;
 
-            if (m_Debug) { //Calculate the points for the triangle in the mesh that we have hit with our raycast.
+            if (debug) { //Calculate the points for the triangle in the mesh that we have hit with our raycast.
                 if (mesh) {
-                    m_TrianglePoint0 = hit.collider.transform.TransformPoint(mesh.vertices[mesh.triangles[hit.triangleIndex * 3 + 0]]);
-                    m_TrianglePoint1 = hit.collider.transform.TransformPoint(mesh.vertices[mesh.triangles[hit.triangleIndex * 3 + 1]]);
-                    m_TrianglePoint2 = hit.collider.transform.TransformPoint(mesh.vertices[mesh.triangles[hit.triangleIndex * 3 + 2]]);
+                    trianglePoint0 = hit.collider.transform.TransformPoint(mesh.vertices[mesh.triangles[hit.triangleIndex * 3 + 0]]);
+                    trianglePoint1 = hit.collider.transform.TransformPoint(mesh.vertices[mesh.triangles[hit.triangleIndex * 3 + 1]]);
+                    trianglePoint2 = hit.collider.transform.TransformPoint(mesh.vertices[mesh.triangles[hit.triangleIndex * 3 + 2]]);
                 }
             }
 
@@ -133,25 +133,25 @@ public class Footsteps : MonoBehaviour {
 
             // Grass is none
             // Gravel is alpha
-            m_Grass = 1.0f - vertexColor.a;
-            m_Gravel = vertexColor.a;
+            grassAmount = 1.0f - vertexColor.a;
+            gravelAmount = vertexColor.a;
         } else {
             //If the ray hits somethign other than the ground, we assume it hit a wooden prop (This is specific to the Viking Village scene) - and set the parameter values for wood.
-            m_Grass = 0.0f;
-            m_Gravel = 0.0f;
+            grassAmount = 0.0f;
+            gravelAmount = 0.0f;
         }
 
-        if (m_Debug)
-            Debug.Log("Grass: " + m_Grass + " Gravel: " + m_Gravel); // + " Sand: " + m_Sand + " Water: " + m_Water);
+        if (debug)
+            Debug.Log("Grass: " + grassAmount + " Gravel: " + gravelAmount); // + " Sand: " + Sand + " Water: " + Water);
 
-        if (m_EventPath != null) {
-            FMOD.Studio.EventInstance e = FMODUnity.RuntimeManager.CreateInstance(m_EventPath);
+        if (eventPath != null) {
+            FMOD.Studio.EventInstance e = FMODUnity.RuntimeManager.CreateInstance(eventPath);
             e.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
 
-            e.setParameterByName("Grass", m_Grass);
-            e.setParameterByName("Gravel", m_Gravel);
-            // e.setParameterByName("Sand", m_Sand);
-            // e.setParameterByName("Water", m_Water);
+            e.setParameterByName("Grass", grassAmount);
+            e.setParameterByName("Gravel", gravelAmount);
+            // e.setParameterByName("Sand", Sand);
+            // e.setParameterByName("Water", Water);
 
             e.start();
             e.release(); //Release each event instance immediately, there are fire and forget, one-shot instances.
---
+++ Project Show Off/Assets/Scripts/Runtime/Misc/EnableColliderOnPlay.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Runtime {
+    public class EnableColliderOnPlay : MonoBehaviour {
+        [SerializeField] private Collider collider;
+
+        private void Awake() {
+            collider.enabled = true;
+        }
+    }
+}
