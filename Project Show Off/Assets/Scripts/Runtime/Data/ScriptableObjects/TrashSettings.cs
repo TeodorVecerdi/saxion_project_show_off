6e9e8fb53d52c2bf694066648a6932d81e251bcb
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-06-21 18:45:51 +0300

    Make prefabs work with navigation AI

---
+++ Project Show Off/Assets/Scripts/Runtime/Data/ScriptableObjects/TrashSettings.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Runtime.Data {
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class TrashSettings : ScriptableObject {
+        
+    }
+}
