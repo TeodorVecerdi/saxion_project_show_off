a1d5e7ed89a9daff7d3936b76584f95bb31896e4
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-06-23 19:18:37 +0300

    Add all prefabs, add preloader scene

---
+++ Project Show Off/Assets/Scripts/Runtime/Misc/FadeOutOnAwake.cs
@@ -0,0 +1,14 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runtime {
+    public class FadeOutOnAwake : MonoBehaviour {
+        [SerializeField] private Graphic target;
+        [SerializeField] private float duration = 0.5f;
+
+        private void Awake() {
+            target.DOFade(0.0f, duration).SetUpdate(true);
+        }
+    }
+}
