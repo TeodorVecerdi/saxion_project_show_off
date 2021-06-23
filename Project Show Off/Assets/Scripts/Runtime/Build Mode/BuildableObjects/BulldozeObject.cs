f739ac538c941e5427333ec32a262e4ce19b47e4
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-06-23 17:30:27 +0300

    Add bulldozing

---
+++ Project Show Off/Assets/Scripts/Runtime/Build Mode/BuildableObjects/BulldozeObject.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Runtime {
+    public class BulldozeObject : MonoBehaviour {
+        [SerializeField] private GameObject root;
+        [SerializeField] private GameObject previewObject;
+        [SerializeField] private Material previewMaterial;
+
+        [SerializeField] private float animationDuration = 0.25f;
+        [SerializeField] private float materialAlpha = 0.5f;
+
+        public void HideIndicator() {
+            previewMaterial.DOFade(0.0f, animationDuration)
+                           .OnComplete(() => previewObject.SetActive(false));
+        }
+
+        public void ShowIndicator() {
+            previewObject.SetActive(true);
+            previewMaterial.DOFade(materialAlpha, animationDuration);
+        }
+
+        public void Bulldoze() {
+            Destroy(root);
+        }
+    }
+}
